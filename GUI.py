387cec08bfecc7d3124cf43b1270349b9c6ffaf4
Jerry Spice <36059502+Jerry-Spice@users.noreply.github.com>
2022-12-15 23:12:52 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import filedialog
 
 from wordProcessor import *
 from teleprompter import *
@@ -13,12 +14,18 @@ class Window(object):
         self.fileLabel = tk.Label(self.root, font=("Arial", 15), text="Enter the file name for the script (include the file extentsion '.docx') and then press the button to continue.")
         self.fileEntry = tk.Entry(self.root, font=("Arial", 15), width=50, justify="center")
         self.startWordProcessorButton = tk.Button(self.root, font=("Arial", 15), text="Process File", command=self.run_button)
-
+        self.fileExplorerButton = tk.Button(self.root, font=("Arial", 15), text="Browse Files", command=self.browse_files)
+        
         self.fileLabel.place(relx=0.5, y=25, anchor="center")
         self.fileEntry.place(relx=0.5, y=70, anchor="center")
-        self.startWordProcessorButton.place(relx=0.5,y=120, anchor="center")
+        self.fileExplorerButton.place(relx=0.5, y=120, anchor="center")
+        self.startWordProcessorButton.place(relx=0.5,y=170, anchor="center")
 
         self.quitting = False
+
+    def browse_files(self):
+        self.filename = filedialog.askopenfilename(initialdir = "/", title = "Select a File", filetypes = (("Word files",".docx"), ("All Files", "*.*")))
+        self.fileEntry.insert(0, self.filename)
 
     def run_button(self):
         parse_script(self.fileEntry.get())
